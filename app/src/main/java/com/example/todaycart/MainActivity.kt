766376d3833ce9fa46a7eccc258ca8b125de5fe7
sondong <ndn153@naver.com>
2023-05-22 15:11:30 +0900

    test2

--- app/src/main/java/com/example/todaycart/MainActivity.kt
+++ app/src/main/java/com/example/todaycart/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         val btnLogin : Button = findViewById(R.id.btnLogin)
         val btnJoin : Button = findViewById(R.id.btnJoin)
         val rc : RecyclerView = findViewById(R.id.rc1)
-        var member : MutableList<MemberVO> = mutableListOf()
+        var members : MutableList<MemberVO> = mutableListOf()
 
         btnLogin.setOnClickListener {
             val intent = Intent(this@MainActivity,LoginActivity::class.java)
@@ -24,7 +25,9 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity,JoinActivity::class.java)
             startActivity(intent)
         }
-        member.add(MemberVO(R.drawable.ad))
-
+        members.add(MemberVO(R.drawable.ad))
+        val adapter = MemberAdapter(applicationContext,R.layout.ad_list,members)
+        rc.layoutManager = LinearLayoutManager(applicationContext,LinearLayoutManager.VERTICAL,false)
+        rc.adapter = adapter
     }
 }
