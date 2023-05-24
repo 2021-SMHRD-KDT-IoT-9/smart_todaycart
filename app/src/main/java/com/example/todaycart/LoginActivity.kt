e4f388defcad03609db84a0d402d8cb65f79cebe
윤해강 <122777546+Yoonhaegang@users.noreply.github.com>
2023-05-24 15:26:06 +0900

    서버연결 시도

--- app/src/main/java/com/example/todaycart/LoginActivity.kt
+++ app/src/main/java/com/example/todaycart/LoginActivity.kt
@@ -1,33 +1,74 @@
 package com.example.todaycart
 
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var queue: RequestQueue
+    private lateinit var etId: EditText
+    private lateinit var etPw: EditText
+    private lateinit var btnDoLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val etId : EditText = findViewById(R.id.etId)
-        val etPw : EditText = findViewById(R.id.etPw)
-        val btnDoLogin : Button = findViewById(R.id.btnDoLogin)
+        etId = findViewById(R.id.etId)
+        etPw = findViewById(R.id.etPw)
+        btnDoLogin = findViewById(R.id.btnDoLogin)
 
+        queue = Volley.newRequestQueue(applicationContext)
 
         btnDoLogin.setOnClickListener {
-           val intent = Intent(this@LoginActivity,Main_AfterActivity::class.java)
-            intent.putExtra("id", etId.text.toString())
-            intent.putExtra("pw", etPw.text.toString())
-            startActivity(intent)
+            val url = "http://119.200.31.135:9090/project/loginCheckMember"
 
-//            finish()
-        }
+            val id = etId.text.toString()
+            val pw = etPw.text.toString()
+            if (id.isNotEmpty() && pw.isNotEmpty()) {
+                val params = HashMap<String, String>()
+                params["id"] = id
+                params["pw"] = pw
 
+                val request = object : StringRequest(
+                    Request.Method.POST,
+                    url,
+                    Response.Listener<String> { response ->
+                        val json = JSONObject(response)
+                        val success = json.getBoolean("success")
+                        val message = json.getString("message")
+
+                        if (success) {
+                            val intent = Intent(this@LoginActivity, Main_AfterActivity::class.java)
+                            intent.putExtra("id", id)
+                            intent.putExtra("pw", pw)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        }
+                    },
+                    Response.ErrorListener {
+                        Toast.makeText(this, "서버 통신 실패", Toast.LENGTH_SHORT).show()
+                    }
+                ) {
+                    override fun getParams(): Map<String, String> {
+                        return params
+                    }
+                }
+
+                queue.add(request)
+            } else {
+                Toast.makeText(this, "아이디/비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
+}
