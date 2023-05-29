7639532aeefbb4f1583a8558d7dda4a44f669425
sondong <ndn153@naver.com>
2023-05-30 08:54:58 +0900

    ad update ver7

--- app/src/main/java/com/example/todaycart/ShoppingActivity.kt
+++ app/src/main/java/com/example/todaycart/ShoppingActivity.kt
@@ -3,13 +3,17 @@ package com.example.todaycart
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.PrintStreamPrinter
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 class ShoppingActivity : AppCompatActivity() {
-
     lateinit var cost1 : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,15 +21,15 @@ class ShoppingActivity : AppCompatActivity() {
         val rcv1 : RecyclerView = findViewById(R.id.rcv1)
         val rcv2 : RecyclerView = findViewById(R.id.rcv2)
         val btnAd : Button = findViewById(R.id.btnAd)
+        val btnPayment : Button = findViewById(R.id.btnPayment)
         cost1 = findViewById(R.id.cost1)
-        cost1.text = "hi"
 
-        val products : MutableList<ProductVO> = mutableListOf()
-        val ads : MutableList<AdVO> = mutableListOf()
+        val products = ArrayList<ProductVO>()
+        val ads = ArrayList<AdVO>()
+
         ads.add(AdVO(R.drawable.cida,"칠성사이다","1800"))
         ads.add(AdVO(R.drawable.beer,"테라","2000"))
         ads.add(AdVO(R.drawable.pepsi,"펩시제로","1800"))
-
         products.add(ProductVO(R.drawable.snack,"오!감자(오리지널)","2000"))
 
         val adapter = ProductAdapter(this ,R.layout.ad_list,products)
@@ -38,10 +42,10 @@ class ShoppingActivity : AppCompatActivity() {
             rcv2.adapter=adapter2
         }
 
-
     }
     fun changeCost(cost:Int){
         cost1.text = cost.toString()
-//        cost1.text = "hello"
     }
+
+
 }
