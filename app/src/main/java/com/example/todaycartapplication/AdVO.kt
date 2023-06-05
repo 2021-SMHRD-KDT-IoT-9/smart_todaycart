e0e3b7074e276d08e20d6c0a9940ce2a567b9d0a
최의현 <114298234+choieuihyun97@users.noreply.github.com>
2023-06-05 16:36:13 +0900

    Add files via upload
    디자인 세부수정 23.06.05.16:35

---
+++ app/src/main/java/com/example/todaycartapplication/AdVO.kt
@@ -0,0 +1,7 @@
+package com.example.todaycartapplication
+
+data class AdVO (
+    val img3 : Int = 0,
+    val name : String = "",
+    val cost : String = ""
+)
---
+++ app/src/main/java/com/example/todaycartapplication/ProductVO.kt
@@ -0,0 +1,9 @@
+package com.example.todaycartapplication
+
+data class ProductVO (
+    val img2 : Int = 0,
+    val name : String = "",
+    val cost : String = "",
+    val location : String = "",
+    val button: String = ""
+        )
